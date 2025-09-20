8ce2d9bcda60502308f37436af77fcd39b99966e
Laiba Irshad <laibashad71@gmail.com>
2025-09-20 02:59:40 -0700

    2nd task

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
+import Faith from "./components/Faith";
 import ScoreCard from "./components/scoreCard";
 const App = () => {
   return (
     <>
-<div className="flex flex-row gap-4 w-full bg-red-200">
+{/* <div className="flex flex-row gap-4 w-full bg-red-200">
       <ScoreCard post="Total Members" score={12345}  />
       <ScoreCard post="Total Post" score={1234} />
       <ScoreCard post="Total Comments" score={1234} />
       <ScoreCard post="Server Load" score={24.23} />
       
-</div>
+</div> */}
+<Faith/>
     </>
   );
 };
---
+++ src/components/Faith.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Faith = () => {
+  return (
+
+    <div className="bg-purple-100">
+        <h1 className="pl-40 font-bold text-3xl font-mono">Faith</h1>
+        <div className="flex justify-content bg-white h-80 w-60 rounded-3xl ">
+      <img src="https://www.publicdomainpictures.net/pictures/260000/velka/quran.jpg"
+       alt="Quran" className="w-40 h-40"
+      ></img>
+      <span className="pt-60 font-semibold text-2xl font-semi">QURAN RECITATION</span>
+    </div>
+    </div>
+  );
+};
+
+export default Faith;
